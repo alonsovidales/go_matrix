ca202408debe3ce7a191dc378bbd4e643b9cd970
Alonso Vidales <alonso.vidales@tras2.es>
2013-12-25 10:05:38 +0100

    Added some functions to work with elements of the matixs

--- matrix.go
+++ matrix.go
@@ -37,12 +37,28 @@ func Mult(m1 [][]float64, m2 [][]float64) (result [][]float64) {
 	result = make([][]float64, len(m1))
 	// Initialize the matrix
 	for x := 0; x < len(m1); x++ {
-		result[x] = make([]float64, len(m1))
+		result[x] = make([]float64, len(m2[0]))
 
-		for y := 0; y < len(m1); y++ {
+		for y := 0; y < len(m2[0]); y++ {
 			for k := 0; k < len(m2); k++ {
 				result[x][y] += m1[x][k] * m2[k][y]
 			}
+		}
+	}
+
+	return
+}
+
+// Returns as result a matrix with all the elemtns of the first one multiplyed
+// by the second one elems
+func MultElems(m1 [][]float64, m2 [][]float64) (result [][]float64) {
+	result = make([][]float64, len(m1))
+	// Initialize the matrix
+	for x := 0; x < len(m1); x++ {
+		result[x] = make([]float64, len(m1[0]))
+
+		for y := 0; y < len(m1[0]); y++ {
+			result[x][y] = m1[x][y] * m2[x][y]
 		}
 	}
 
@@ -59,6 +75,33 @@ func Trans(m1 [][]float64) (result [][]float64) {
 
 		for y := 0; y < len(m1); y++ {
 			result[x][y] = m1[y][x]
+		}
+	}
+
+	return
+}
+
+// Sum all the elements in a matrix
+func SumAll(m [][]float64) (result float64) {
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[0]); j++ {
+			result += m[i][j]
+		}
+	}
+
+	return
+}
+
+// Apply a function to all the elements of a matrix, the function will receive a
+// float64 as param and return a float64 too
+func Apply(m [][]float64, f func(x float64) (float64)) (result [][]float64) {
+	result = make([][]float64, len(m))
+
+	// Initialize the matrix
+	for x := 0; x < len(m); x++ {
+		result[x] = make([]float64, len(m[0]))
+		for y := 0; y < len(m[0]); y++ {
+			result[x][y] = f(m[x][y])
 		}
 	}
 
--- matrix_test.go
+++ matrix_test.go
@@ -57,6 +57,32 @@ func TestMatrixSum(t *testing.T) {
 	}
 }
 
+func TestMultElems(t *testing.T) {
+	m1 := [][]float64{
+		[]float64{3, 2, 1},
+		[]float64{9, 5, 7},
+	}
+	m2 := [][]float64{
+		[]float64{2, 3, 4},
+		[]float64{1, 4, 7},
+	}
+
+	expectedRes := [][]float64{
+		[]float64{6, 6, 4},
+		[]float64{9, 20, 49},
+	}
+
+	result := MultElems(m1, m2)
+
+	for i := 0; i < len(result); i++ {
+		for j := 0; j < len(result); j++ {
+			if result[i][j] != expectedRes[i][j] {
+				t.Error("Expected result on pos:", i, j, ":", expectedRes[i][j], "but obtained:", result[i][j])
+			}
+		}
+	}
+}
+
 func TestMatrixSub(t *testing.T) {
 	m1 := [][]float64{
 		[]float64{3, 2, 1},
@@ -96,6 +122,45 @@ func TestMatrixTrans(t *testing.T) {
 	}
 
 	result := Trans(m1)
+
+	for i := 0; i < len(result); i++ {
+		for j := 0; j < len(result[0]); j++ {
+			if result[i][j] != expectedRes[i][j] {
+				t.Error("Expected result on pos:", i, j, ":", expectedRes[i][j], "but obtained:", result[i][j])
+			}
+		}
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	m := [][]float64{
+		[]float64{3, 2, 1},
+		[]float64{9, 5, 7},
+	}
+
+	result := SumAll(m)
+
+	if result != 27 {
+		t.Error("Expected result for SumAll: 27 but obtained:", result)
+	}
+}
+
+func TestApply(t *testing.T) {
+	m := [][]float64{
+		[]float64{4, 2, 1},
+		[]float64{8, 3, 6},
+	}
+
+	expectedRes := [][]float64{
+		[]float64{2, 1, 0.5},
+		[]float64{4, 1.5, 3},
+	}
+
+	f := func(x float64) float64 {
+		return x / 2
+	}
+
+	result := Apply(m, f)
 
 	for i := 0; i < len(result); i++ {
 		for j := 0; j < len(result[0]); j++ {
